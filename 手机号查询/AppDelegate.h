7b5c5ca239980659bb0128e9088ab71583f8eaf6
582324909 <582324909@qq.com>
2016-11-22 09:37:34 +0800

    Initial Commit

---
+++ 手机号查询/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  手机号查询
+//
+//  Created by 张伟伟 on 2016/11/22.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 手机号查询/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  手机号查询
+//
+//  Created by 张伟伟 on 2016/11/22.
+//  Copyright © 2016年 张伟伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
